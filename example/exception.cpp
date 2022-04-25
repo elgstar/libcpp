53848fca1b57100f9ccc6707311620f85477ef3b
elgstar <jxndsongliangjin@outlook.com>
2022-04-26 07:04:50 +0800

    utility

--- example/exception.cpp
+++ example/exception.cpp
@@ -1,23 +1,23 @@
 /*
  * the exception test
 */
-#include "../lib/exception.hpp"
+#include "../kits/exception.hpp"
 
 #include <exception>
 
 int main()
 {
     std::string msg{"test!"};
-    try{
+    try {
         throw EXCEPTION(msg);
         // throw ERRORLOGIC(msg);
         // throw ERROREXPECTION(msg);
         // throw ERROROUTFLOW(msg);
     }
-    catch (slj::Exception & e){
+    catch(slj::Exception &e) {
         e.view();
     }
-    catch(std::exception & e){
+    catch(std::exception &e) {
         std::cerr << e.what() <<std::endl;
     }
 }
--- example/utility.cpp
+++ example/utility.cpp
@@ -1,4 +1,4 @@
-#include "../lib/utility.hpp"
+#include "../kits/utility.hpp"
 #include <iostream>
 
 template <typename R>
@@ -14,8 +14,16 @@ void print(const R &a, const R &b)
 
 int main()
 {
-    print<float>(0, 0.00000000001);
-    print<float>(0.0000000001, 0);
+    print<slj::n_real>(0, 0.00000000001);
+    print<slj::n_real>(0.0000000001, 0);
     print<float>(0.000001, 0);
     print<float>(0.00001, 0);
+    slj::n_real a = 1.5;
+    slj::n_nat  b = 10;
+    double c = 1.5;
+    size_t d = 10;
+    std::cout<<typeid(a).name()<<"      "<<sizeof(slj::n_real)<<std::endl;
+    std::cout<<typeid(b).name()<<"      "<<sizeof(slj::n_nat)<<std::endl;
+    std::cout<<typeid(c).name()<<"      "<<sizeof(double)<<std::endl;
+    std::cout<<typeid(d).name()<<"      "<<sizeof(size_t)<<std::endl;
 }
